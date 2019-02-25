8d095cb325a7d84fdbf8dafdb869b7964df0d9db
Ievgen Gusar <ievgen_gusar@epam.com>
2019-02-25 23:25:05 +0200

    Added additional solved tasks

---
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2113/Hippodrome.java
@@ -0,0 +1,55 @@
+package com.javarush.task.task21.task2113;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Hippodrome {
+    private List<Horse> horses;
+    public static  Hippodrome game;
+
+    public Hippodrome() {
+    }
+
+    public Hippodrome(List<Horse> horses) {
+        this.horses = horses;
+    }
+
+    public List<Horse> getHorses() {
+        return horses;
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        List<Horse> horses = new ArrayList<>();
+        horses.add(new Horse("Bella", 3, 0));
+        horses.add(new Horse("Angella", 3, 0));
+        horses.add(new Horse("Argo", 3, 0));
+        game = new Hippodrome(horses);
+        game.run();
+    }
+    public void run() {
+        for (int i = 0; i < 100; i++) {
+            move();
+            print();
+            try {
+                Thread.sleep(200);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void move() {
+        for (Horse horse : horses) {
+            horse.move();
+        }
+    }
+
+    public void print() {
+        for (Horse horse : horses) {
+            horse.print();
+        }
+        for (int i = 0; i < 10; i++) {
+            System.out.println();
+        }
+    }
+}
