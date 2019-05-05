c79152dbda040effde8b9724450fb1dfa43e2e3a
Ievgen Gusar <ievgen_gusar@epam.com>
2019-05-05 21:31:02 +0300

    Added additional solved tasks

---
+++ 3.JavaMultithreading/src/com/javarush/task/task28/task2805/MyThread.java
@@ -0,0 +1,49 @@
+package com.javarush.task.task28.task2805;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class MyThread extends Thread {
+    private static AtomicInteger priority = new AtomicInteger(MIN_PRIORITY);
+
+    {
+        if (priority.get() < MAX_PRIORITY) {
+            setPriority(priority.getAndIncrement());
+        } else {
+            setPriority(priority.get());
+            priority.set(1);
+        }
+    }
+
+    public MyThread() {
+        super();
+    }
+
+    public MyThread(ThreadGroup group, String name) {
+        super(group, name);
+    }
+
+
+    public MyThread(Runnable target) {
+        super(target);
+    }
+
+    public MyThread(ThreadGroup group, Runnable target) {
+        super(group, target);
+    }
+
+    public MyThread(String name) {
+        super(name);
+    }
+
+    public MyThread(Runnable target, String name) {
+        super(target, name);
+    }
+
+    public MyThread(ThreadGroup group, Runnable target, String name) {
+        super(group, target, name);
+    }
+
+    public MyThread(ThreadGroup group, Runnable target, String name, long stackSize) {
+        super(group, target, name, stackSize);
+    }
+}
